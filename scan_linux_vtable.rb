fe889c7b95778f03ce7b96f7307173657ed8fb98
jj <jj@jj>
2012-05-30 23:35:16 +0200

    scan_linux_vtable: fix output, double-check vfuncptrs, signal conflicts in output

--- scan_linux_vtable.rb
+++ scan_linux_vtable.rb
@@ -2,12 +2,12 @@ require 'metasm'
 
 puts 'read file' if $VERBOSE
 binpath = ARGV.shift || 'libs/Dwarf_Fortress'
-elf = Metasm::AutoExe.decode_file(binpath).disassembler
+dasm = Metasm::AutoExe.decode_file(binpath).disassembler
 
 puts 'scan *st' if $VERBOSE
 strings = {}
-elf.pattern_scan(/\d+\w+st\0/) { |addr|
-	strings[addr] = elf.decode_strz(addr)
+dasm.pattern_scan(/\d+\w+st\0/) { |addr|
+	strings[addr] = dasm.decode_strz(addr)
 }
 
 def demangle_str(s)
@@ -15,27 +15,85 @@ def demangle_str(s)
 	s[len.length, len.to_i]
 end
 
-bin_dwords = File.read(binpath).unpack('L*')
 
-puts 'scan sptr' if $VERBOSE
+Metasm::DynLdr.new_func_c <<EOC
+#line #{__LINE__}
+// return the index in str where it contains one of ptrs
+// starts from the end, iterate by restarting at last offset found
+int scanptrs(char *str, int strlen, unsigned *ptrs, int ptrslen) {
+	int i;
+	unsigned p;
+	for (i=strlen-4 ; i ; --i) {
+		p = *(unsigned*)(str+i);
+		if (p < ptrs[0] || p > ptrs[ptrslen-1])
+			continue;
+		for (int j=0 ; j<ptrslen ; ++j)
+			if (p == ptrs[j])
+				return i;
+	}
+	return -1;
+}
+EOC
+
+def scanptrs(raw, hash)
+	off = raw.length
+	ptrs = hash.keys.sort.pack('L*')
+	ret = []
+	loop do
+		off = Metasm::DynLdr.scanptrs(raw, off, ptrs, hash.length)
+		break if off < 0
+		ret << [off, hash[raw[off, 4].unpack('L')[0]]]
+	end
+	ret
+end
+
+
+
+
+# linux vtable:
+
+# vtable-4  typeinfoptr
+# vtable    vfunc0
+# vtable+4  vfunc1
+# ...
+#
+# typeinfoptr   parent?
+# typeinfoptr+4 mangled_classname_ptr
+
+
+# find all pointers to what looks like mangled structure name ("06unitst")
+file_raw = File.read(binpath)
+
 sptr = {}
-bin_dwords.each_with_index { |dw, dw_idx|
-	if str = strings.delete(dw)
-		vaddr = elf.fileoff_to_addr(dw_idx*4)
-		sptr[vaddr-4] = str
+scanptrs(file_raw, strings).each { |off, str|
+	vaddr = dasm.fileoff_to_addr(off) - 4
+	sptr[vaddr] = str
+}
+
+# find [address, length] of the .text section
+text = dasm.section_info.find { |n, a, l, i| n == '.text' }.values_at(1, 2)
+
+# vtable 
+vtable = {}
+scanptrs(file_raw, sptr).each { |off, str|
+	vaddr = dasm.fileoff_to_addr(off) + 4
+
+	# check that we have an actual function pointer here (eg into .text)
+	vf = dasm.decode_dword(vaddr)
+	next if vf < text[0] or vf > text[0]+text[1]
+
+	s = demangle_str(str)
+	if vtable[s]
+		vtable[s] += ' or 0x%x' % vaddr
+	else
+		vtable[s] = '0x%x' % vaddr
 	end
 }
 
-puts 'scan vtable' if $VERBOSE
-xml = []
-bin_dwords.each_with_index { |dw, dw_idx|
-	if str = sptr.delete(dw)
-		vaddr = elf.fileoff_to_addr(dw_idx*4)
-		vaddr += 4
-		xml << "<vtable-address name='#{demangle_str(str)}' value='0x#{'%x' % vaddr}'/>"
+vtable.sort.each { |str, vaddr|
+	if vaddr =~ / or /
+		puts "<!-- CONFLICT vtable-address name='#{str}' value='#{vaddr}'/ -->"
+	else
+		puts "<vtable-address name='#{str}' value='#{vaddr}'/>"
 	end
 }
-
-puts xml.sort
-
-#puts "not found:", strings.values.sort, sptr.values
