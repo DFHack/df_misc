8f279f9e4107f365a3b707b7d65125376a80243c
jj <jj@jj>
2012-08-23 18:18:28 +0200

    scanstartdwarfcount: say it works on all versions, add more comments on whats going on

--- scan_startdwarfcount.rb
+++ scan_startdwarfcount.rb
@@ -1,7 +1,14 @@
 require 'metasm'
 
 # this scripts scans a binary for the number 7 used as starting dwarf count in fortress mode
-abort 'usage: scan <exe> <sizeofunit>' if ARGV.length != 2
+# works for the windows/linux and macos versions
+
+# needs the size of the struct unit for the current version, can be found in
+#  build/plugins/ruby-autogen.rb right after 'class Unit' once dfhack is compiled
+# (only for the current build target, linux is the same as mac, but differs from windows)
+
+abort 'usage: scan <exe> <sizeof_unitstruct>' if ARGV.length != 2
+
 ENV['METASM_NODECODE_RELOCS'] = '1'
 
 exe = Metasm::AutoExe.decode_file(ARGV.shift)
@@ -15,43 +22,58 @@ dasm = exe.disassembler
 
 # find all 0x00000007 in the binary
 puts 'scan 7' if $VERBOSE
-o7 = dasm.pattern_scan([7].pack('L'))
+off_7 = dasm.pattern_scan([7].pack('L'))
 
 # find all 0xsizeofunit in the binary
 puts 'scan sizeof unit' if $VERBOSE
-ou = dasm.pattern_scan([sizeofunit].pack('L'))
+off_sz = dasm.pattern_scan([sizeofunit].pack('L'))
 
 # search close couples
 puts 'proximity scan' if $VERBOSE
-may = o7.find_all { |a7| ou.find { |au| au > a7 and au < a7 + 32 } }
+candidates_7 = off_7.find_all { |a7| off_sz.find { |au| au > a7 and au < a7 + 32 } }
 
-p ou, may if $DEBUG
+p off_sz, candidates_7 if $DEBUG
 
 # look for the loop condition thing
-may.each { |o|
-	# find the instruction containing '7' as 2nd argument
-	adi = o - 2
+candidates_7.each { |addr_7|
+	# scan for the instruction containing '7' as 2nd argument
+	di_addr = addr_7 - 2
 	di = nil
 	8.times { |i|
-		di = dasm.disassemble_instruction(adi)
+		di = dasm.disassemble_instruction(di_addr)
 		break if di and di.instruction.args[1] == Metasm::Expression[7]
-		adi -= 1
+		di_addr -= 1
 		di = nil
 	}
 
 	next if not di
 	puts di if $VERBOSE
 
-	# disassemble from there
-	dasm.disassemble_fast adi
+	# disassemble from this instruction
+	dasm.disassemble_fast di_addr
 
-	# find where 7 is loaded
+	# find register/memory 7 is stored
+	# eg mov eax, 7  =>  eax
 	store = di.instruction.args[0].to_s
 
-	# find loop condition
-	block7 = dasm.block_at(adi)
-	endaddrs = block7.to_normal.map { |to| dasm.block_at(to).from_normal }.flatten - [block7.list.last.address]
-	if endaddrs.find { |ea| dasm.block_at(ea).list[-2].instruction.args[0].to_s == store }
-		puts "<global-address name='start_dwarf_count' value='0x%x' fileoff='0x%x'/>" % [o, dasm.addr_to_fileoff(o)]
+	# check loop condition: loop_body is the block following 'di',
+	#  find the blocks jumping back to loop_body and different from the current block (loop_init)
+	#  the end-of-loop test is the next-to-last instr of these blocks
+	#  if the loop test depends on 'store', assume we found it
+	loop_init = dasm.block_at(di_addr)
+
+	# take all addresses jumping to the blocks following loop_init
+	endaddrs = loop_init.to_normal.map { |loop_body_addr|
+		dasm.block_at(loop_body_addr).from_normal
+	}.flatten - [loop_init.list.last.address]
+
+	# search if the next-to-last instr uses 'store', eg  cmp eax, 0  jnz loop_body
+	ebl = nil
+	if endaddrs.find { |ea|
+		ebl = dasm.block_at(ea)
+		ebl.list[-2].instruction.args[0].to_s == store
+	}
+		puts ebl.list[-2, 2] if $VERBOSE
+		puts "<global-address name='start_dwarf_count' value='0x%x' fileoff='0x%x'/>" % [addr_7, dasm.addr_to_fileoff(addr_7)]
 	end
 }
