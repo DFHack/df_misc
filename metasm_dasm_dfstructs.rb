6849286eed154993935684d4c8d277aa05451350
jj <john-git@ofjj.net>
2012-12-06 19:44:26 +0100

    metasm_dasm_struct: autoinvoke magic after every disassembly

--- metasm_dasm_dfstructs.rb
+++ metasm_dasm_dfstructs.rb
@@ -3,52 +3,41 @@
 # quickstart:
 #  install ruby, and ruby-gtk if on linux
 #  create a directory with metasm/, Dwarf_Fortress, globals.csv
-#  edit the 'ep' line below, and run
 #
-# ruby scan_vtable.rb --map Dwarf_Fortress > globals.map
-# ruby globalcsv2map.rb globals.csv >> globals.map
-# perl codegen_c_hdr.pl dfhack/library/include/df/codegen.out.xml --stdc --linux codegen.h
-# ruby -I metasm metasm/samples/disassemble-gui.rb Dwarf_Fortress -P metasm_dasm_dfstructs
+# ruby scan_vtable.rb --map Dwarf_Fortress > Dwarf_Fortress.map
+# ruby globalcsv2map.rb globals.csv >> Dwarf_Fortress.map
+# perl codegen_c_hdr.pl dfhack/library/include/df/codegen.out.xml --stdc --linux Dwarf_Fortress.h
+# mv metasm_dasm_dfstructs.rb Dwarf_Fortress.rb
+# ruby -I metasm metasm/samples/disassemble-gui.rb Dwarf_Fortress -a
 
 # to navigate, use 'g' to go to a specific address
 # use ctrl-maj-C to disassemble
 # use 't' to define a register as a pointer to a struct
-#  eg click on 'eax' somewhere, type 't', and type 'unitst' in the popup
-# use 'K' to name stack variables
+#  eg click on 'eax' somewhere, type 't', and type 'itemst' in the popup
+# use 'K' to scan for stack variables for current function
+# use 'n' to rename labels/variables
 # use 'k' to toggle string representation / raw numeric value
 # 'space' toggles graph view
 
-# drawbacks: no save
+# Warning:
 # dont disassemble too many things
 # dont type 'tab' (will take hours trying to decompile and fail)
 
 
-# linux 34.11 ctor_viewscreen_layer_militaryst
-ep = 0x08cd9290
-
-load_map 'globals.map'
-parse_c_file 'codegen.h'
-
-disassemble_fast_deep(ep)
-gui.focus_addr(ep, :graph)
-
-load_plugin 'imm2off'		# add xrefs to known labels (from map file)
-load_plugin 'stringsxrefs'	# add comments on instrs with pointers to ascii
-load_plugin 'demangle_cpp'	# add demangled C++ names in comments
-load_plugin 'hl_opcode'
-
 
 # load globals.csv, map label names -> struct name
-globals = File.readlines('globals.csv').inject({}) { |h, l|
+$df_globals = File.readlines(File.dirname(program.filename) + '/globals.csv').inject({}) { |h, l|
 	u = l.split(',').map { |w| w[1...-1] }
 	next h if u[6].empty?
 	h.update u[5].gsub(/[^\w]/, '_') => u[6]
 }
 
-count = 0
 
-# propagate type info, from globals and vtable_*
-prog_binding.each { |name, addr|
+def df_structs
+    count = 0
+
+    # propagate type info, from globals and vtable_*
+    prog_binding.each { |name, addr|
 	if name =~ /^vtable_(\w+)/
 		struct = $1
 		each_xref(addr) { |xr|
@@ -71,7 +60,7 @@ prog_binding.each { |name, addr|
 			end
 		}
 
-	elsif struct = globals[name]
+	elsif struct = $df_globals[name]
 		each_xref(addr) { |xr|
 			if di = di_at(xr.origin) and di.to_s =~ /mov (\w+), \[#{name}\]/
 				reg = $1.to_sym
@@ -82,6 +71,23 @@ prog_binding.each { |name, addr|
 		}
 
 	end
+    }
+
+    puts "tweaked #{count} instrs with df types"
+end
+
+
+# run all this whenever we finish disassembling some new code
+last_cnt = 0
+self.callback_finished  = proc {
+	if last_cnt != @decoded.length
+		last_cnt = @decoded.length
+		load_plugin 'imm2off'
+		load_plugin 'stringsxrefs'
+		load_plugin 'demangle_cpp'
+		df_structs
+	end
 }
 
-puts "tweaked #{count} instrs with df types"
+# also run now
+self.callback_finished[]
