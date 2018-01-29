89c5deaae388a895675610e27c27a6341a9c6733
jj <john-git@ofjj.net>
2018-01-29 16:00:09 +0100

    dump_globals: add function for dfhack name translation

--- dump_df_globals.rb
+++ dump_df_globals.rb
@@ -8,16 +8,8 @@ require 'metasm'
 dump_fmt = 'xml'
 dump_fmt = 'idc' if ARGV.delete '--idc'
 
-dfhack_names = {
-	"version" => "version",
-	"min_load_version" => "min_load_version",
-	"movie_version" => "movie_version",
-	"basic_seed" => "basic_seed",
-	"enabler" => "enabler",
-	"cursor" => "cursor",
+Dfhack_names = {
 	"point" => "selection_rect",
-	"gamemode" => "gamemode",
-	"gametype" => "gametype",
 	"menuposition" => "ui_menu_width",
 	"itemmade" => "created_item_type",
 	"itemmade_subtype" => "created_item_subtype",
@@ -25,14 +17,8 @@ dfhack_names = {
 	"itemmade_subcat2" => "created_item_matindex",
 	"itemmade_number" => "created_item_count",
 	"mainview" => "map_renderer",
-	"d_init" => "d_init",
-	"title" => "title",
 	"title2" => "title_spaced",
 	"event_flow" => "flows",
-	"gps" => "gps",
-	"gview" => "gview",
-	"init" => "init",
-	"texture" => "texture",
 	"plot_event" => "timed_events",
 	"plotinfo" => "ui",
 	"adventure" => "ui_advmode",
@@ -43,7 +29,6 @@ dfhack_names = {
 	"buildjob_item" => "ui_building_assign_items",
 	"looklist" => "ui_look_list",
 	"game" => "ui_sidebar_menus",
-	"world" => "world",
 	"year" => "cur_year",
 	"season_count" => "cur_year_tick",
 	"precise_phase" => "cur_year_tick_advmode",
@@ -110,49 +95,29 @@ dfhack_names = {
 	"standingorder_gatherrefuse_outside" => "standing_orders_gather_refuse_outside",
 	"standingorder_gatherrefuse_outside_vermin" => "standing_orders_gather_vermin_remains",
 	"standingorder_gatherwood" => "standing_orders_gather_wood",
-	"option_exceptions" => "standing_orders_job_cancel_announce",
 	"standingorder_mixfoods" => "standing_orders_mix_food",
 	"standingorder_dyed_clothes" => "standing_orders_use_dyed_cloth",
 	"standingorder_zone_drinking" => "standing_orders_zoneonly_drink",
 	"standingorder_zone_fishing" => "standing_orders_zoneonly_fish",
-	"next_activity_global_id" => "activity_next_id",
-	"next_agreement_global_id" => "agreement_next_id",
-	"next_army_controller_global_id" => "army_controller_next_id",
-	"next_army_global_id" => "army_next_id",
-	"next_army_tracking_info_global_id" => "army_tracking_info_next_id",
+	"option_exceptions" => "standing_orders_job_cancel_announce",
 	"next_art_imagechunk_global_id" => "art_image_chunk_next_id",
-	"next_artifact_global_id" => "artifact_next_id",
-	"next_belief_system_global_id" => "belief_system_next_id",
-	"next_building_global_id" => "building_next_id",
-	"next_crime_global_id" => "crime_next_id",
-	"next_cultural_identity_global_id" => "cultural_identity_next_id",
-	"next_dance_form_global_id" => "dance_form_next_id",
 	"next_civ_global_id" => "entity_next_id",
-	"next_flow_guide_global_id" => "flow_guide_next_id",
-	"next_formation_global_id" => "formation_next_id",
 	"next_histeventcol_global_id" => "hist_event_collection_next_id",
 	"next_histevent_global_id" => "hist_event_next_id",
 	"next_histfig_global_id" => "hist_figure_next_id",
-	"next_identity_global_id" => "identity_next_id",
-	"next_incident_global_id" => "incident_next_id",
-	"next_interaction_instance_global_id" => "interaction_instance_next_id",
-	"next_item_global_id" => "item_next_id",
-	"next_job_global_id" => "job_next_id",
-	"next_machine_global_id" => "machine_next_id",
-	"next_musical_form_global_id" => "musical_form_next_id",
 	"next_nem_global_id" => "nemesis_next_id",
-	"next_occupation_global_id" => "occupation_next_id",
-	"next_poetic_form_global_id" => "poetic_form_next_id",
-	"next_proj_global_id" => "proj_next_id",
-	"next_rhythm_global_id" => "rhythm_next_id",
-	"next_scale_global_id" => "scale_next_id",
-	"next_schedule_global_id" => "schedule_next_id",
-	"next_squad_global_id" => "squad_next_id",
 	"next_unitchunk_global_id" => "unit_chunk_next_id",
-	"next_unit_global_id" => "unit_next_id",
-	"next_vehicle_global_id" => "vehicle_next_id",
-	"next_written_content_global_id" => "written_content_next_id",
 }
+
+def dfhack_names(n)
+	Dfhack_names[n] or case n
+	when /^next_(.*)_global_id/
+		$1 + '_next_id'
+	#when /^standingorder/
+	else
+		n
+	end
+end
 
 ENV['METASM_NODECODE_RELOCS'] = '1'
 binpath = ARGV.shift || 'Dwarf Fortress.exe'
@@ -186,8 +151,7 @@ while true
 	ptr_var = dasm.decode_dword(off)
 	off += bits/8
 	break if ptr_str == 0
-	name = dfhack_names.fetch(dasm.decode_strz(ptr_str), nil)
-	next if not name
+	name = dfhack_names(dasm.decode_strz(ptr_str))
 
 	case dump_fmt
 	when 'xml'
