8697165f880e978e63a4e7e23401e24f80739746
Ben Lubar <ben.lubar@gmail.com>
2020-01-15 23:12:46 -0600

    I checked manually and the function call is always the first one.

--- scan_twbt.rb
+++ scan_twbt.rb
@@ -7,7 +7,6 @@ vtoff = Integer(ARGV.shift)
 
 ENV['METASM_NODECODE_RELOCS'] = '1'
 dasm = Metasm::AutoExe.decode_file(binpath).disassembler
-$dasm = dasm
 
 $ptrsz = 4
 $ptrsz = 8 if dasm.cpu.size == 64
@@ -15,8 +14,7 @@ $ptrsz = 8 if dasm.cpu.size == 64
 # render method
 render = dasm.normalize(dasm.decode_dword(vtoff + 2 * $ptrsz))
 
-def find_calls(addr, max_off)
-    dasm = $dasm
+def find_calls(dasm, addr)
     dasm.disassemble_fast(addr)
 
     targets = []
@@ -25,7 +23,6 @@ def find_calls(addr, max_off)
         ldi = dasm.block_at(baddr).list.last
 
         next if ldi.opcode.name != 'call'
-        break if baddr - addr >= max_off
 
         targets << dasm.resolve(ldi.instruction.args[0])
     }
@@ -33,10 +30,6 @@ def find_calls(addr, max_off)
     return targets
 end
 
-render_map = find_calls(render, 50)
-if render_map.length != 1
-    puts "<!-- CONFLICT global-address name='twbt_render_map' value='#{render_map.map { |a| '0x%08x' % a }.join(' or ')}'/ -->"
-else
-    puts "<global-address name='twbt_render_map' value='0x#{'%08x' % render_map[0]}'/>"
-end
+render_map = find_calls(render)[0]
+puts "<global-address name='twbt_render_map' value='0x#{'%08x' % render_map}'/>"
 
