b6ed7f9263d8b1b5bcb060e1078b1d7e0c3b5f24
Kelly Kinkade <kelly.lynn.martin@gmail.com>
2023-05-08 12:00:10 -0500

    more fixconflicts improvements

--- ghidra/FixConflicts.java
+++ ghidra/FixConflicts.java
@@ -11,6 +11,8 @@ import util.CollectionUtils;
 import java.util.List;
 import java.util.stream.Stream;
 import java.util.stream.Collectors;
+import java.time.Duration;
+import java.time.Instant;
 
 import ghidra.app.script.GhidraScript;
 import ghidra.program.model.mem.*;
@@ -29,25 +31,26 @@ import ghidra.util.Swing;
 
 public class FixConflicts extends GhidraScript {
 
+	static int maxPerRun = 1000;
+
 	@Override
 	protected void run() throws Exception {
-		boolean rerun = false;
 		DataTypeManager dtm = currentProgram.getDataTypeManager();
 
-		List<DataType> conflicts = CollectionUtils.asStream(dtm.getAllDataTypes())
-			.filter(dt -> dt.getName().contains(".conflict"))
-			.collect(Collectors.toList());
-		int orig = conflicts.size();
-		String message = "Fixing Conflicts " + String.valueOf(conflicts.size()) + " of " + String.valueOf(orig);
-		println(message);
 		int id = dtm.startTransaction("Fixing Conflicts");
-		boolean success = false;
 		try {
+			List<DataType> conflicts = CollectionUtils.asStream(dtm.getAllDataTypes())
+				.filter(dt -> dt.getName().contains(".conflict"))
+				.collect(Collectors.toList());
+
+			String message = "Fixing " + String.valueOf(conflicts.size()) + " conflicts...";
+			println(message);
+			Instant start = Instant.now();
 			monitor.initialize(conflicts.size());
 			monitor.setMessage(message);
 			for (DataType dt : conflicts) {
 				monitor.checkCanceled();
-				monitor.setMessage(message + ":" + dt.getName());
+				monitor.setMessage(dt.getName());
 				if (dt.isDeleted()) {
 					monitor.incrementProgress(1);
 					continue;
@@ -67,10 +70,16 @@ public class FixConflicts extends GhidraScript {
 				dtm.replaceDataType(dt, good, false);
 				monitor.incrementProgress(1);
 				Swing.allowSwingToProcessEvents();
+				Duration elapsed = Duration.between(start, Instant.now());
+				if (elapsed.toMillis() > 60000)
+				{
+					dtm.endTransaction(id, true);
+					id = dtm.startTransaction("Fixing Conflicts");
+					start = Instant.now();
+				}
 			}
-			success = true;
 		} finally {
-			dtm.endTransaction(id, success);
+			dtm.endTransaction(id, true);
 		}
 	}
 
